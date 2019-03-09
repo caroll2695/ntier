15ac13132f508e2f2fd45acaa5a69e8e7c3aae96
Nilesh Londhe <nilesh@cloudgeni.us>
2019-03-08 21:43:32 -0800

    edits to address https://github.com/docker/for-linux/issues/591

--- terraform/nat-server.tf
+++ terraform/nat-server.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "nat" {
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu trusty stable'",
       "sudo apt-get update",
-      "sudo apt-get -y install docker-ce=17.03.3~ce-0~ubuntu-xenial",
+      "sudo apt-get -y install docker-ce=18.06.1~ce~3-0~ubuntu",
 
       /* Initialize open vpn data container */
       "sudo mkdir -p /etc/openvpn",
